c072d664d7a89ff90e9a98f57bf7319502c89961
James-Alex Matthews <vamtiger@vamtiger.com>
2017-11-21 08:44:24 +0200

    vamtiger-node-typescript-commit: Test

--- build/commit/index.js
+++ build/commit/index.js
@@ -9,33 +9,38 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const vamtiger_bash_1 = require("vamtiger-bash");
+const path_1 = require("path");
 const __1 = require("..");
 let commitMessage = 'vamtiger-node-typescript-commit';
 function commit(options) {
     return __awaiter(this, void 0, void 0, function* () {
         const test = options.test;
         const updateVersion = options.updateVersion ? options.updateVersion : __1.UpdateVersion.patch;
+        const repositoryPath = options.repositoryPath ? options.repositoryPath : process.argv[1];
         const sourceBranch = options.sourceBranch ? options.sourceBranch : 'source';
         const masterBranch = options.masterBranch ? options.masterBranch : 'master';
-        const sourceFolder = options.sourceFolder ? options.sourceFolder : __1.Folder.source;
-        const buildFolder = options.buildFolder ? options.buildFolder : __1.Folder.build;
+        const sourceFolder = options.sourceFolder ? options.sourceFolder : path_1.resolve(repositoryPath, __1.Folder.source);
+        const buildFolder = options.buildFolder ? options.buildFolder : path_1.resolve(repositoryPath, __1.Folder.build);
         const runScript = options.runScript ? options.runScript : __1.RunScript.npm;
         const buildScript = options.buildScript ? options.buildScript : __1.BuildScript.build;
         const push = options.push ? true : false;
         const publish = push && options.publish ? true : false;
+        const bashOptions = {
+            cwd: repositoryPath
+        };
         const message = test ? `${commitMessage}: Test` : commitMessage;
-        const checkoutSource = yield vamtiger_bash_1.default(`git checkout ${sourceBranch}`);
-        const removeBuild = yield vamtiger_bash_1.default('rm -rfv build');
-        const status = yield vamtiger_bash_1.default('git status');
+        const checkoutSource = yield vamtiger_bash_1.default(`git checkout ${sourceBranch}`, bashOptions);
+        const removeBuild = yield vamtiger_bash_1.default('rm -rfv build', bashOptions);
+        const status = yield vamtiger_bash_1.default('git status', bashOptions);
         const addSource = yield vamtiger_bash_1.default('git add -A');
-        const commitSource = yield vamtiger_bash_1.default(`git commit -m "${message}"`);
-        const checkoutMaster = yield vamtiger_bash_1.default(`git checkout ${masterBranch}`);
-        const mergeFromSource = yield vamtiger_bash_1.default(`git merge -X theirs ${sourceBranch}`);
-        const build = yield vamtiger_bash_1.default(`${runScript} ${buildScript}`);
-        const removeRedundantSource = yield vamtiger_bash_1.default(`rm -rfv yarn.lock tsconfig .vscode ${sourceFolder}`);
-        const addBuild = yield vamtiger_bash_1.default('git add -A');
-        const commitBuild = yield vamtiger_bash_1.default(`git commit -m "${message}"`);
-        const update = yield vamtiger_bash_1.default(`npm version ${updateVersion}`);
+        const commitSource = yield vamtiger_bash_1.default(`git commit -m "${message}"`, bashOptions);
+        const checkoutMaster = yield vamtiger_bash_1.default(`git checkout ${masterBranch}`, bashOptions);
+        const mergeFromSource = yield vamtiger_bash_1.default(`git merge -X theirs ${sourceBranch}`, bashOptions);
+        const build = yield vamtiger_bash_1.default(`${runScript} ${buildScript}`, bashOptions);
+        const removeRedundantSource = yield vamtiger_bash_1.default(`rm -rfv yarn.lock tsconfig .vscode ${sourceFolder}`, bashOptions);
+        const addBuild = yield vamtiger_bash_1.default('git add -A', bashOptions);
+        const commitBuild = yield vamtiger_bash_1.default(`git commit -m "${message}"`, bashOptions);
+        const update = yield vamtiger_bash_1.default(`npm version ${updateVersion}`, bashOptions);
         let pushUpdate;
         let publishUpdate;
         if (push)
--- source/commit/index.ts
+++ source/commit/index.ts
@@ -1,4 +1,6 @@
 import bash from 'vamtiger-bash';
+import { PathLike } from 'fs';
+import { resolve as resolvePath} from 'path';
 import { Options, UpdateVersion, Folder, RunScript, BuildScript } from '..';
 
 let commitMessage = 'vamtiger-node-typescript-commit';
@@ -6,27 +8,31 @@ let commitMessage = 'vamtiger-node-typescript-commit';
 export default async function commit(options: Options) {
     const test = options.test;
     const updateVersion = options.updateVersion ? options.updateVersion : UpdateVersion.patch;
+    const repositoryPath = options.repositoryPath ? options.repositoryPath : process.argv[1];
     const sourceBranch = options.sourceBranch ? options.sourceBranch : 'source';
     const masterBranch = options.masterBranch ? options.masterBranch : 'master';
-    const sourceFolder = options.sourceFolder ? options.sourceFolder : Folder.source;
-    const buildFolder = options.buildFolder ? options.buildFolder : Folder.build;
+    const sourceFolder = options.sourceFolder ? options.sourceFolder : resolvePath(repositoryPath, Folder.source);
+    const buildFolder = options.buildFolder ? options.buildFolder : resolvePath(repositoryPath, Folder.build);
     const runScript = options.runScript ? options.runScript : RunScript.npm;
     const buildScript = options.buildScript ? options.buildScript : BuildScript.build;
     const push = options.push ? true : false;
     const publish = push && options.publish ? true : false;
+    const bashOptions = {
+        cwd: repositoryPath as string
+    };
     const message = test ? `${commitMessage}: Test`: commitMessage;
-    const checkoutSource = await bash(`git checkout ${sourceBranch}`);
-    const removeBuild = await bash('rm -rfv build');
-    const status = await bash('git status');
+    const checkoutSource = await bash(`git checkout ${sourceBranch}`, bashOptions);
+    const removeBuild = await bash('rm -rfv build', bashOptions);
+    const status = await bash('git status', bashOptions);
     const addSource = await bash('git add -A');
-    const commitSource = await bash(`git commit -m "${message}"`);
-    const checkoutMaster = await bash(`git checkout ${masterBranch}`);
-    const mergeFromSource = await bash(`git merge -X theirs ${sourceBranch}`);
-    const build = await bash(`${runScript} ${buildScript}`);
-    const removeRedundantSource = await bash(`rm -rfv yarn.lock tsconfig .vscode ${sourceFolder}`);
-    const addBuild = await bash('git add -A');
-    const commitBuild = await bash(`git commit -m "${message}"`);
-    const update = await bash(`npm version ${updateVersion}`);
+    const commitSource = await bash(`git commit -m "${message}"`, bashOptions);
+    const checkoutMaster = await bash(`git checkout ${masterBranch}`, bashOptions);
+    const mergeFromSource = await bash(`git merge -X theirs ${sourceBranch}`, bashOptions);
+    const build = await bash(`${runScript} ${buildScript}`, bashOptions);
+    const removeRedundantSource = await bash(`rm -rfv yarn.lock tsconfig .vscode ${sourceFolder}`, bashOptions);
+    const addBuild = await bash('git add -A', bashOptions);
+    const commitBuild = await bash(`git commit -m "${message}"`, bashOptions);
+    const update = await bash(`npm version ${updateVersion}`, bashOptions);
 
     let pushUpdate: string[];
     let publishUpdate: string;
