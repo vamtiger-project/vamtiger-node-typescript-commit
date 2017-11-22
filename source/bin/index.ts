36d3eb38420cb5696c49ef8454b5792023ad7156
James-Alex Matthews <vamtiger@vamtiger.com>
2017-11-23 00:41:33 +0200

    Build

---
+++ source/bin/index.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+import commit from '..';
+import { RunScript } from '..';
+
+export default commit;
+
+const Args = require('vamtiger-argv');
+const args = new Args();
+const runScript = args.has('runscript') ? args.get('runscript') as RunScript : RunScript.npm;
+const repositoryPath = process.cwd();
+const push = args.has('push') as boolean;
+const publish = args.has('publish') as boolean;
+const params = {
+    runScript,
+    repositoryPath,
+    push,
+    publish
+};
+
+main().catch(error => handleError({error}));
+
+async function main() {
+    const commitChanges = await commit(params);
+}
+
+function handleError(params: IHandleErrorParams) {
+    const error = params.error;
+
+    console.warn(error.message);
+    console.warn(error.stack);
+    
+    throw error;
+}
+
+interface IHandleErrorParams {
+    error: Error;
+}
