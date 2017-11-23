04f164b63420067de1ea098d90ebeca2854e1054
James-Alex Matthews <vamtiger@vamtiger.com>
2017-11-23 09:01:24 +0200

    Removed Build

--- build/test/01-commit-node-typescript-project/test-node-typescript-repository/source/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare const _default: "2017 November 23 - 01:21:25.500";
-export default _default;
--- build/test/01-commit-node-typescript-project/test-node-typescript-repository/source/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = "2017 November 23 - 01:21:25.500";
-//# sourceMappingURL=index.js.map
